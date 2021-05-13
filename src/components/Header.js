3f64132d282615c40cc6b12790cfc0c1d7375d7a
Esmeray Tasgit <estasgit@gmail.com>
2021-05-13 23:05:07 +0200

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <h1 className="main-heading">
-    <img className="spotify_logo" src="https://image.flaticon.com/icons/png/512/49/49097.png" alt="logo" width="65px"></img>
+    <img className="spotify_logo" src="https://image.flaticon.com/icons/png/512/49/49097.png" alt="logo" width="70px"></img>
       <span className="spotify">Spotify</span>
       <span className="search">Search</span>
     </h1>
