4baadfb4505766492d5fafa82e73a319289b0b76
Esmeray Tasgit <estasgit@gmail.com>
2021-05-13 22:10:48 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-  return <h1 className="main-heading">Spotify Music Search</h1>;
+  return (
+    <h1 className="main-heading">
+    <img className="spotify_logo" src="https://image.flaticon.com/icons/png/512/49/49097.png" alt="logo" width="6%"></img>
+      <span className="spotify">Spotify</span>
+      <span className="search">Search</span>
+    </h1>
+  );
 };
 
 export default Header;
